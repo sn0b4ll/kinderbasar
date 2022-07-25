4a31fbf6cdb86704f8ad64843b627dabca6caffc
sn0b4ll <dominik.schlecht@hotmail.de>
2022-07-25 23:46:41 +0200

    Added login logic and session handling.

--- routes.py
+++ routes.py
@@ -4,12 +4,21 @@ import json
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect, url_for
+from flask import session
 from flask_sqlalchemy import SQLAlchemy
+
+from werkzeug.exceptions import BadRequestKeyError
 
 from flask_qrcode import QRcode
 
 app = Flask(__name__)
+app.secret_key = 'set_me_to_something_random_please'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+'''
+---- Database ----
+'''
 
 db = SQLAlchemy(app)
 QRcode(app)
@@ -19,13 +28,38 @@ class Article(db.Model):
     name = db.Column(db.String)
     seller = db.Column(db.Integer)
     price = db.Column(db.String)
-    sold = db.Column(db.Boolean) # Was boolean, not working on development environment at least..
+    sold = db.Column(db.Boolean)
 
     def __repr__(self):
         return f"<Article(uuid='{self.uuid}', name='{self.name}', seller='{self.seller}', price='{self.price}, sold='{self.sold}'"
 
+class User(db.Model):
+    uuid = db.Column(db.String, primary_key=True)
+    secret = db.Column(db.String)
+    organizer = db.Column(db.Boolean)
+
 db.create_all()
 
+user = User()
+user.uuid = str(uuid.uuid4())
+user.secret = "abcd"
+user.organizer = False
+db.session.add(user)
+
+# Test-Data
+article = Article()
+article.uuid = str(uuid.uuid4())
+article.name = "Testname"
+article.seller = user.uuid
+article.price = "13.37"
+article.sold = False
+db.session.add(article)
+
+db.session.commit()
+
+'''
+---- Routes ----
+'''
 @app.route("/")
 def home():
     return render_template(
@@ -34,12 +68,41 @@ def home():
         description="Description"
     )
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        uuid = request.form['username']
+        secret = request.form['password']
+
+        
+        user = User.query.get(uuid)
+        if (user is not None) and (user.secret == secret):
+            session['seller_uuid'] = user.uuid
+            session['organizer'] = user.organizer
+            return redirect(url_for('seller'))
+        return "Nope"
+
+    else:
+        return render_template(
+            'login.html',
+            title="Login"
+        )
+
+@app.route("/logout")
+def logout():
+    session.pop('seller_uuid')
+    session.pop('organizer')
+    return redirect(url_for('login'))
+
 @app.route("/add_article")
 def add_article():
-    return render_template(
-        'add_article.html',
-        title="Add an article"
-    )
+    if 'seller_uuid' in session:
+        return render_template(
+            'add_article.html',
+            title="Add an article"
+        )
+    else:
+       return redirect(url_for('login'))
 
 @app.route("/article", methods=["GET", "POST"])
 def article():
@@ -52,7 +115,7 @@ def article():
         article = Article()
         article.uuid = str(uuid.uuid4())
         article.name = name
-        article.seller = "Seller"
+        article.seller = session['seller_uuid']
         article.price = price
         article.sold = False
     
@@ -63,14 +126,21 @@ def article():
         #return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
         return render_template(
             'qr_code.html',
-            url=url
+            url=url, 
+            article=article
         )
     else:
         args = request.args
         article = Article.query.filter_by(uuid=args['uuid']).first()
+        if ('organizer' in session) and (session['organizer'] == True):
+            org = True
+        else:
+            org = False
+        print("Org:" + str(org))
         return render_template(
             'article.html',
-            article=article
+            article=article,
+            org=org
         )
 
 @app.route("/article/<string:uuid>/sold", methods=["POST"])
@@ -83,21 +153,13 @@ def article_sold(uuid):
 
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
-print("test")
+@app.route("/seller", methods=["GET"])
+def seller():
+    articles = Article.query.filter_by(seller=session['seller_uuid'])
 
-article = Article()
-article.uuid = str(uuid.uuid4())
-article.name = "Testname"
-article.seller = "Seller"
-article.price = "13.37"
-article.sold = False
-
-db.session.add(article)
-db.session.commit()
-
-for article in db.session.query(Article).all():
-    print(article)
-    if article.sold:
-        print("Was sold!")
+    return render_template(
+            'seller.html',
+            articles=articles
+        )
 
 db.session.close()
