75158e42ec12870315944b18b018a31ecb35ae4c
sn0b4ll <dominik.schlecht@hotmail.de>
2022-07-25 22:49:49 +0200

    Extended basic func to creation, scanning and selling

--- routes.py
+++ routes.py
@@ -4,13 +4,27 @@ import json
 from flask import Flask
 from flask import render_template
 from flask import request
-from model import session, Article
+from flask_sqlalchemy import SQLAlchemy
 
 from flask_qrcode import QRcode
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
+db = SQLAlchemy(app)
 QRcode(app)
+
+class Article(db.Model):
+    uuid = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String)
+    seller = db.Column(db.Integer)
+    price = db.Column(db.String)
+    sold = db.Column(db.Boolean) # Was boolean, not working on development environment at least..
+
+    def __repr__(self):
+        return f"<Article(uuid='{self.uuid}', name='{self.name}', seller='{self.seller}', price='{self.price}, sold='{self.sold}'"
+
+db.create_all()
 
 @app.route("/")
 def home():
@@ -18,6 +32,13 @@ def home():
         'home.html',
         title="Jinja Demo Site",
         description="Description"
+    )
+
+@app.route("/add_article")
+def add_article():
+    return render_template(
+        'add_article.html',
+        title="Add an article"
     )
 
 @app.route("/article", methods=["GET", "POST"])
@@ -33,14 +54,34 @@ def article():
         article.name = name
         article.seller = "Seller"
         article.price = price
-        article.sold = "False"
+        article.sold = False
     
-        session.add(article)
-        session.commit()
-        return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
-    else:
-        return "Article Backend"
+        db.session.add(article)
+        db.session.commit()
 
+        url = "http://192.168.1.36:5000/article?uuid=" + article.uuid
+        #return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+        return render_template(
+            'qr_code.html',
+            url=url
+        )
+    else:
+        args = request.args
+        article = Article.query.filter_by(uuid=args['uuid']).first()
+        return render_template(
+            'article.html',
+            article=article
+        )
+
+@app.route("/article/<string:uuid>/sold", methods=["POST"])
+def article_sold(uuid):
+    article = Article.query.filter_by(uuid=uuid).first()
+    article.sold = True
+    article.price = request.form['price']
+
+    db.session.commit()
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
 print("test")
 
@@ -49,14 +90,14 @@ article.uuid = str(uuid.uuid4())
 article.name = "Testname"
 article.seller = "Seller"
 article.price = "13.37"
-article.sold = "False"
+article.sold = False
 
-session.add(article)
-session.commit()
+db.session.add(article)
+db.session.commit()
 
-for article in session.query(Article).all():
+for article in db.session.query(Article).all():
     print(article)
-    if article.sold == "True":
+    if article.sold:
         print("Was sold!")
 
-session.close()
+db.session.close()
