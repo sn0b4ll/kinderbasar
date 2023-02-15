29c45938c7051edb747c28ce2d7d46a6859f134c
Dominik Schlecht <dominik.schlecht@hotmail.de>
2023-02-15 01:07:31 +0100

    Added logout test.

--- tests/test_login.py
+++ tests/test_login.py
@@ -13,7 +13,7 @@ config.read('./conf/env.conf')
 # Create a requests session
 req_session = requests.Session()
 
-def test_login():
+def test_login_logout():
     '''Is the login working?'''
     post_data = { # TODO(Config for test-data?)
         'username': 'seller1@kinderbasar-elsendorf.de',
@@ -23,3 +23,21 @@ def test_login():
     response = req_session.post(f"{ config['APP']['url'] }/login", data = post_data)
     assert response.status_code is 200
     assert 'logout' in response.text
+
+def test_logout():
+    '''Is the login working?'''
+
+    # Check if login is still there
+    response = req_session.get(f"{ config['APP']['url'] }/overview")
+    assert response.status_code is 200
+    assert 'Artikel Übersicht' in response.text
+
+    # Try to log out
+    response = req_session.get(f"{ config['APP']['url'] }/logout")
+    assert response.status_code is 200
+    assert 'Login' in response.text
+
+    # Try to call something after logging out
+    response = req_session.get(f"{ config['APP']['url'] }/overview")
+    assert response.status_code is 200
+    assert 'Artikel Übersicht' not in response.text
